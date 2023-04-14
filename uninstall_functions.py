eb3b35c20b8c6d6fea36b4d8c299848fd501bc94
Shashi Raj <98850848+EmperialX@users.noreply.github.com>
2023-04-15 04:29:34 +0530

    Update uninstall_functions.py

--- uninstall_functions.py
+++ uninstall_functions.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python
+import os
 
-def uninstall_shortcut(shortcut_path):
+# Define a function to remove the shortcut files
+def remove_shortcut(shortcut_name):
+    shortcut_path = os.path.expanduser(f"~/shortcuts/{shortcut_name}.txt")
+    os.remove(shortcut_path)
+    print(f"Shortcut '{shortcut_name}' removed.")
+
+# Define a function to remove the PowerShell functions from the profile
+def remove_shortme_runme():
+    profile_path = os.path.expanduser("~/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1")
+    with open(profile_path, "r") as f:
+        lines = f.readlines()
+    new_lines = [l for l in lines if "shortme" not in l and "runme" not in l]
+    with open(profile_path, "w") as f:
+        f.writelines(new_lines)
+    print("PowerShell functions removed from the profile.")
+
+# Remove the functions and shortcut files
+remove_shortcut("MyShortcut")
+remove_shortme_runme()
+
+# Print a thank you message
+print("Thank you!")
+
